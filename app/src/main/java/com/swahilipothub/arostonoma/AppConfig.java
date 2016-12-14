4af941d485bad1e8926a7a349f3a26f55245e262
Kevin Barasa <kevin.barasa001@gmail.com>
2016-12-14 19:33:14 +0300

    Added Volley to perform Register and Login

---
+++ app/src/main/java/com/swahilipothub/arostonoma/AppConfig.java
@@ -0,0 +1,13 @@
+package com.swahilipothub.arostonoma;
+
+/**
+ * Created by Kevin Barassa on 14-Dec-16.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://arostonoma.co.ke/users/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://arostonoma.co.ke/users/register.php";
+}
