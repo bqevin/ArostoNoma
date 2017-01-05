3561557a0c4a16a8f6e04059bea3ca0c382b397d
Kevin Barasa <kevin.barasa001@gmail.com>
2017-01-05 22:28:49 +0300

    SwipetoRefresh for new articles

--- app/src/main/java/com/swahilipothub/arostonoma/RehabActivity.java
+++ app/src/main/java/com/swahilipothub/arostonoma/RehabActivity.java
@@ -1,29 +1,23 @@
 package com.swahilipothub.arostonoma;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Cache;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.swahilipothub.arostonoma.app.AppController;
 
 import org.json.JSONArray;
@@ -46,6 +40,8 @@ public class RehabActivity extends AppCompatActivity {
     private static final String URL_DATA = "http://arostonoma.co.ke/articles.json";
     //For snackbar
     private CoordinatorLayout coordinatorLayout;
+    //Swipe to refresh
+    SwipeRefreshLayout refresherL;
 
 
     @Override
@@ -59,6 +55,9 @@ public class RehabActivity extends AppCompatActivity {
         //Enable snackbar viewing
         coordinatorLayout = (CoordinatorLayout) findViewById(R.id
                 .coordinatorLayout);
+
+        //Swipe refresh layout init
+        refresherL = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
 
         //Ensures every item on recycler view has fixed size
         recyclerView.setHasFixedSize(true);
@@ -91,131 +90,32 @@ public class RehabActivity extends AppCompatActivity {
             }
 
         } else {
-            // making fresh volley request and getting json
-            JsonObjectRequest jsonReq = new JsonObjectRequest(Request.Method.GET,
-                    URL_DATA, null, new Response.Listener<JSONObject>() {
-
-                @Override
-                public void onResponse(JSONObject response) {
-                    //VolleyLog.d(TAG, "Response: " + response.toString());
-                    if (response != null) {
-                        parseJsonFeed(response);
-                    }
-                }
-            }, new Response.ErrorListener() {
-
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                   // VolleyLog.d(TAG, "Error: " + error.getMessage());
-                    //Dismiss dialog
-                    p.dismiss();
-
-                    //Enable SnackBar
-                    Snackbar snackbar = Snackbar
-                            .make(coordinatorLayout, "No internet connection!", Snackbar.LENGTH_LONG)
-                            .setAction("RETRY", new View.OnClickListener() {
-                                @Override
-                                public void onClick(View view) {
-                                }
-                            });
-
-                    // Changing message text color
-                    snackbar.setActionTextColor(Color.RED);
-
-                    // Changing action button text color
-                    View sbView = snackbar.getView();
-                    TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
-                    textView.setTextColor(Color.YELLOW);
-                    snackbar.show();
-
-                }
-            });
-
-            // Adding request to volley request queue
-            AppController.getInstance().addToRequestQueue(jsonReq);
+            //Make completely new request
+            //Activate dialog
+            p.setMessage("Fetching Articles");
+            p.show();
+            newArticleRequest();
         }
-    }
 
-    //Fetch articles
-    private void loadArticles() {
-        StringRequest stringRequest = new StringRequest(Request.Method.GET,
-                URL_DATA,
-                new Response.Listener<String>() {
+        /*
+         *
+         * Refreshes Articles
+         */
+        refresherL.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
                     @Override
-                    public void onResponse(String response) {
-
-                        p.dismiss();
-
-                        //MUST be in the try catch clause
-                        try {
-
-                            //Get the Object
-                            JSONObject jsonObj = new JSONObject(response);
-
-                            // Get Array
-                            JSONArray recipeArray = jsonObj.getJSONArray("articles");
-                            for (int i = 0; i < recipeArray.length(); i++ ) {
-
-                                //fetch individual objects
-                                JSONObject o = recipeArray.getJSONObject(i);
-
-                                //Article objects
-                                RehabArticles item = new RehabArticles();
-
-                                item.setAuthor(o.getString("author"));
-                                item.setTitle(o.getString("title"));
-                                item.setImage(o.getString("image"));
-                                item.setBody(o.getString("body"));
-                                item.setTime(o.getString("time"));
-                                articles.add(item);
-                            }
-
-                            //Create adapter object
-                            adapter = new RehabAdapter(articles, getApplicationContext());
-
-                            //Set adapter
-                            recyclerView.setAdapter(adapter);
-
-
-                        } catch (JSONException e){
-                            //Volley Error
-                            e.printStackTrace();
-                        }
-
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        //Dismiss dialog
-                        p.dismiss();
-
-                        //Enable SnackBar
-                        Snackbar snackbar = Snackbar
-                                .make(coordinatorLayout, "No internet connection!", Snackbar.LENGTH_LONG)
-                                .setAction("RETRY", new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View view) {
-                                    }
-                                });
-
-                        // Changing message text color
-                        snackbar.setActionTextColor(Color.RED);
-
-                        // Changing action button text color
-                        View sbView = snackbar.getView();
-                        TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
-                        textView.setTextColor(Color.YELLOW);
-                        snackbar.show();
-
+                    public void onRefresh() {
+                        // Makes a new network call
+                        newArticleRequest();
+                        //Stop loading animation
+                        refresherL.setRefreshing(false);
                     }
                 });
-        RequestQueue requestQue = Volley.newRequestQueue(this);
-        requestQue.add(stringRequest);
     }
 
+
     /**
-     * Parsing json reponse and passing the data to feed view list adapter
+     * Parsing json reponse and passing the data to Rehab adapter
      * */
     private void parseJsonFeed(JSONObject response) {
         try {
@@ -246,6 +146,51 @@ public class RehabActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    private void newArticleRequest(){
+        // making fresh volley request and getting json
+        JsonObjectRequest jsonReq = new JsonObjectRequest(Request.Method.GET,
+                URL_DATA, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                //VolleyLog.d(TAG, "Response: " + response.toString());
+                if (response != null) {
+                    parseJsonFeed(response);
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // VolleyLog.d(TAG, "Error: " + error.getMessage());
+                //Dismiss dialog
+                p.dismiss();
+
+                //Enable SnackBar
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "No internet connection!", Snackbar.LENGTH_LONG)
+                        .setAction("RETRY", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                            }
+                        });
+
+                // Changing message text color
+                snackbar.setActionTextColor(Color.RED);
+
+                // Changing action button text color
+                View sbView = snackbar.getView();
+                TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+                textView.setTextColor(Color.YELLOW);
+                snackbar.show();
+
+            }
+        });
+
+        // Adding request to volley request queue
+        AppController.getInstance().addToRequestQueue(jsonReq);
     }
 
 
